0d4d58817a37ca2dca7480b3ade5f0b07e3271d3
Omar M <ometoui2000@gmail.com>
2025-02-26 14:54:59 -0500

    Update OneBitAdder.h

--- OneBitAdder.h
+++ OneBitAdder.h
@@ -1,10 +1,12 @@
-module add1 (
+module add1(
     input a,
     input b,
     input cin,
     output sum,
     output cout
 );
-	assign sum = a ^ b ^ cin;
-    assign cout = (a & b) | (a & cin) | (b & cin);
+
+assign sum = a ^ b ^ cin;
+assign cout = (a & b) | (a & cin) | (b & cin);
+
 endmodule
