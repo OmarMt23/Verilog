432afdc9a67c750246a72362dd5283c63ef15259
Omar M <ometoui2000@gmail.com>
2025-02-26 14:52:14 -0500

    Create OneBitAdder.h

---
+++ OneBitAdder.h
@@ -0,0 +1,10 @@
+module add1 (
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
+);
+	assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (a & cin) | (b & cin);
+endmodule
